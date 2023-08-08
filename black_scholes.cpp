33edb1e4aa9783d8bf4ca7284d2dedf712639e93
johnymeng <johnnymeng27@gmail.com>
2023-08-08 16:40:42 -0400

    booles

--- black_scholes.cpp
+++ black_scholes.cpp
@@ -7,3 +7,17 @@
 #include <algorithm>
 
 using namespace std;
+
+//N(0,1) density
+double f(double x)
+{
+    double pi = 4 * atan(1.0);
+    return exp(-x * x * 0.5) / sqrt(2 * pi);
+}
+
+//Boole's Rule
+double Boole(double Start, double End, int n)
+{
+    vector<double> X (n+1, 0.0);
+    vector<double> Y (n+1, 0.0);
+}
